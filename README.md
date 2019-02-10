214f3fbcec61a68778bbee64c58f9df410f6c334
Ronney Vargas <ronneyvargas@gmail.com>
2019-02-10 14:08:59 -0600

    Added functions for moving the items and placing on top of the body.  Also added a function to change the z-index from 5 to 10.

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ Each image is given its own id tag.
 ## Utilized regular events and touch events
 
 ## Reset elements layering positions
+
+Items were assigned a z-index of 5 initially.
+Once the item is dragged the z-index changes from 5 to 10 allowing the items to be placed on top of each other. 
+This was done with the resetZ().
--- script.js
+++ script.js
@@ -4,12 +4,32 @@ var dragndrop = (function() {
     var myY = '';
     var whichArt = '';
 
+    function resetZ() { // will allow to items to be placed on top of each other - changing z-index from 5 to 10
+        var elements = document.querySelectorAll('img');
+        for (var i = elements.length - 1; i >= 0; i--) {
+            elements[i].style.zIndex = 5;
+        };
+    }
+
     function moveStart(e) {
         whichArt = e.target;
         myX = e.offsetX === undefined ? e.layerX : e.offsetX;
         myY = e.offsetY === undefined ? e.layerY : e.offsetY;
+        resetZ();
         whichArt.style.zIndex = 10;
     }
 
+    function moveDragOver(e) {
+        e.preventDefault();
+    }
+
+    function moveDrop(e) {
+        e.preventDefault();
+        whichArt.style.left = e.pageX - myX + 'px';
+        whichArt.style.top = e.pageY - myY + 'px';
+    }
+
     document.querySelector('body').addEventListener('dragstart', moveStart, false);
+    document.querySelector('body').addEventListener('dragover', moveDragOver, false);
+    document.querySelector('body').addEventListener('drop', moveDrop, false);
 })();
